cab517652458cb97f9302d01a494188a128d6e7e
Dave51C <dcoveney@verizon.net>
2026-02-09 18:42:10 -0500

    calibration stuff

---
+++ calibration/calibrate_from_stills.py
@@ -0,0 +1,82 @@
+# $Source: /home/scrobotics/src/2025/calibration/RCS/calibrate_from_stills.py,v $
+# $Revision: 1.3 $
+# $Date: 2025/05/01 19:52:09 $
+# $Author: scrobotics $
+import numpy as np
+import cv2 as cv
+import glob
+import argparse
+from datetime import datetime
+
+
+def write_json (name,width,height,intrinsic,distortion):
+    Cam_Mtx = intrinsic
+    Cam_Dist = distortion
+    # I added "created" because we're getting waaaay too many copies floating around.
+    NOW = "{:%Y-%m-%d:%H:%M:%S}".format(datetime.now())
+    JSON = '{\n   "created": "' + NOW + '",\n'
+    JSON = JSON + '   "name"   : "{0}_{1}x{2}",\n'.format(name,width,height)
+    JSON = JSON + '   "width"  : {0},\n   "height" : {1},\n'.format(width,height)
+    JSON = JSON + '   "mtx"    :\n'
+    JSON = JSON + '   [[{0}, {1}, {2}],\n'.format(Cam_Mtx[0,0],Cam_Mtx[0,1],Cam_Mtx[0,2])
+    JSON = JSON + '    [{0}, {1}, {2}],\n'.format(Cam_Mtx[1,0],Cam_Mtx[1,1],Cam_Mtx[1,2])
+    JSON = JSON + '    [{0}, {1}, {2}]],\n'.format(Cam_Mtx[2,0],Cam_Mtx[2,1],Cam_Mtx[2,2])
+    JSON = JSON + '   "dist"   :\n'
+    JSON = JSON + '   [{0}, {1}, {2},'.format(Cam_Dist[0,0],Cam_Dist[0,1],Cam_Dist[0,2])
+    JSON = JSON + ' {0}, {1}],\n'.format(Cam_Dist[0,3],Cam_Dist[0,4])
+    JSON = JSON + '   "FudgeOffset": 0.0,\n'
+    JSON = JSON + '   "FudgeFactor": 1.0\n'
+    # Leave out detector options
+    #JSON = JSON + '   "DetectorOptions":{"quad_decimate":3, "quad_blur": 0.0, \n "refine_edges": 1, "refine_decode": 1,\n "refine_pose": 0, "quad_contours": 1}'
+    JSON = JSON + '}\n'
+    #J = open(name+'_'+str(width)+'x'+str(height)+'_parameters.json','w')
+    J = open(name+'_'+str(width)+'x'+str(height)+'.json','w')
+    J.write (JSON)
+    J.close ()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--name",help="Camera name",type=str,required=True)
+args = parser.parse_args()
+
+# termination criteria
+criteria   = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp       = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints  = [] # 3d point in real world space
+imgpoints  = [] # 2d points in image plane.
+_img_shape = None
+images = glob.glob('stills/img*.jpg')
+count = 0
+for fname in images:
+    img = cv.imread(fname)
+    if _img_shape == None:
+        _img_shape = img.shape[:2]
+        height = img.shape[0]
+        width  = img.shape[1]
+    else:
+        assert _img_shape == img.shape[:2], "All images must share the same size."
+
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (9,6), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        #print(fname)
+        count += 1
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (9,6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(50)
+    else:
+        print("mv "+fname+" discards")
+    if count > 100:
+        break
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+write_json (args.name,width,height,mtx,dist)
---
+++ calibration/calibration_shots.py
@@ -0,0 +1,129 @@
+import cv2
+import argparse
+import numpy as np
+import sys
+from time import sleep
+
+def open_stream(webcam,width,height):
+    stream = cv2.VideoCapture(webcam)  # Initialize webcam
+    stream.set (cv2.CAP_PROP_FRAME_WIDTH, width) 
+    stream.set (cv2.CAP_PROP_FRAME_HEIGHT,height)
+    print (stream.get(cv2.CAP_PROP_FRAME_WIDTH),"x",stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    return(stream)
+
+def fetch_frame(stream):
+    ret, frame = stream.read()          # Capture frame
+    return(frame)
+
+ONE   = np.array([[450,10],[450,200]],np.int32)
+TWO   = np.array([[350,10],[450,10],[450,100],[350,100],[350,200],[450,200]],np.int32)
+THREE = np.array([[350,10],[450,10],[450,100],[350,100],
+                 [450,100],[450,200],[350,200]],np.int32)
+FOUR  = np.array([[350,10],[350,100],[450,100],[450,10],[450,200]],np.int32)
+FIVE  = np.array([[450,10],[350,10],[350,100],[450,100],[450,200],[350,200]],np.int32)
+
+GREEN  = (0,255,0)
+RED    = (0,0,255)
+CYAN   = (255,255,0)
+YELLOW = (0,255,255)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--width",    type=int, default=640, help="defautl 640")
+parser.add_argument("--height",   type=int, default=480, help="default 480")
+parser.add_argument("--shots",    type=int, default=25,  help="default 25")
+parser.add_argument("--startnum", type=int, default=0,   help="default 0")
+parser.add_argument("--device",   type=int, default=0,   help="default 0")
+args     = parser.parse_args()
+width    = args.width
+height   = args.height
+shots    = args.shots
+startnum = args.startnum
+device   = args.device
+
+cap      = open_stream(device,width,height)
+SEQ      = startnum
+LAST     = startnum + shots
+while SEQ < LAST:
+    T = 30
+    while T > 0:
+        frame = fetch_frame(cap)
+        cv2.polylines(frame, [FIVE], False, RED, 20)
+        cv2.polylines(frame, [FIVE], False, GREEN, 3)
+        if width > 800:
+            scale_x = 0.5
+            scale_y = 0.5
+            smaller = cv2.resize(frame, None, fx=scale_x, fy=scale_y)
+            cv2.imshow('counter', smaller)              # Display frame
+            T -= 3
+        else:
+            cv2.imshow   ('counter',frame)
+        cv2.waitKey(1)
+        T -= 1
+    T = 30
+    while T > 0:
+        frame = fetch_frame(cap)
+        cv2.polylines(frame, [FOUR], False, RED, 20)
+        cv2.polylines(frame, [FOUR], False, GREEN, 3)
+        if width > 800:
+            scale_x = 0.5
+            scale_y = 0.5
+            smaller = cv2.resize(frame, None, fx=scale_x, fy=scale_y)
+            cv2.imshow('counter', smaller)              # Display frame
+            T -= 3
+        else:
+            cv2.imshow   ('counter',frame)
+        cv2.waitKey(1)
+        T -= 1
+    T = 30
+    while T > 0:
+        frame = fetch_frame(cap)
+        cv2.polylines(frame, [THREE], False, RED, 20)
+        cv2.polylines(frame, [THREE], False, GREEN, 3)
+        if width > 800:
+            scale_x = 0.5
+            scale_y = 0.5
+            smaller = cv2.resize(frame, None, fx=scale_x, fy=scale_y)
+            cv2.imshow('counter', smaller)              # Display frame
+            T -= 3
+        else:
+            cv2.imshow   ('counter',frame)
+        cv2.waitKey(1)
+        T -= 1
+    T = 30
+    while T > 0:
+        frame = fetch_frame(cap)
+        cv2.polylines(frame, [TWO], False, RED, 20)
+        cv2.polylines(frame, [TWO], False, GREEN, 3)
+        if width > 800:
+            scale_x = 0.5
+            scale_y = 0.5
+            smaller = cv2.resize(frame, None, fx=scale_x, fy=scale_y)
+            cv2.imshow('counter', smaller)              # Display frame
+            T -= 3
+        else:
+            cv2.imshow   ('counter',frame)
+        cv2.waitKey(1)
+        T -= 1
+    T = 30
+    while T > 0:
+        frame = fetch_frame(cap)
+        cv2.polylines(frame, [ONE], False, RED, 20)
+        cv2.polylines(frame, [ONE], False, GREEN, 3)
+        if width > 800:
+            scale_x = 0.5
+            scale_y = 0.5
+            smaller = cv2.resize(frame, None, fx=scale_x, fy=scale_y)
+            cv2.imshow('counter', smaller)              # Display frame
+            T -= 3
+        else:
+            cv2.imshow   ('counter',frame)
+        cv2.waitKey(1)
+        T -= 1
+
+    frame = fetch_frame(cap)
+    fname = './stills/img' + '{0:04d}'.format(SEQ) + '.jpg'
+    cv2.imwrite (fname,frame)
+    print (fname)
+    SEQ += 1
+    print ('CLICK!')
+
